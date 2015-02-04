Fixed calculation of index of aggreement (Willmott et al. 2005) and added it as a utils function (calc_ioa_d1). Modified qa_temp to use calc_ioa_d1.

bin/step04_mpi_stn_obs_qa.py
@@ -2,7 +2,7 @@
 A MPI script for running Tmin/Tmax quality assurance procedures.
 Must be run using mpiexec or mpirun.
 
-Copyright 2014, Jared Oyler.
+Copyright 2014,2015, Jared Oyler.
 
 This file is part of TopoWx.
 
@@ -25,7 +25,7 @@ import numpy as np
 from twx.db import StationDataDb, STN_ID, TMIN, TMAX, TMIN_FLAG, TMAX_FLAG, STATE
 import twx.qa.qa_temp as qa_temp
 from netCDF4 import Dataset, date2num
-from twx.utils import YMD, status_check, ymdL_to_date, Unbuffered
+from twx.utils import YMD, StatusCheck, ymdL_to_date, Unbuffered
 import sys
 import os
 import twx
@@ -229,7 +229,7 @@ def proc_write(params, nwrkers):
     iter_all = IterMultiFlagUpdate()
 
     nstns = np.nonzero(params[P_STN_MASK])[0].size
-    stat_chk = status_check(nstns, 10)
+    stat_chk = StatusCheck(nstns, 10)
 
     while 1:
 
@@ -285,7 +285,7 @@ def proc_coord(params, nwrkers):
 if __name__ == '__main__':
 
     PROJECT_ROOT = "/projects/topowx"
-    FPATH_STNDATA = os.path.join(PROJECT_ROOT, 'station_data')
+    FPATH_STNDATA = os.path.join(PROJECT_ROOT, 'station_data', 'update_2014')
 
     np.seterr(all='raise')
     np.seterr(under='ignore')
@@ -295,8 +295,8 @@ if __name__ == '__main__':
     nsize = MPI.COMM_WORLD.Get_size()
 
     params = {}
-    params[P_PATH_DB] = os.path.join(FPATH_STNDATA, 'all', 'all_1948_2012.nc')
-    params[P_PATH_POR_OUT] = os.path.join(FPATH_STNDATA, 'all', 'all_por_1948_2012.csv')
+    params[P_PATH_DB] = os.path.join(FPATH_STNDATA, 'all', 'all_1948_2014.nc')
+    params[P_PATH_POR_OUT] = os.path.join(FPATH_STNDATA, 'all', 'all_por_1948_2014.csv')
     
     #Need to run this QA script twice
     #First run, only apply non-spatial QA checks (P_QA_SPATIAL = False)

twx/qa/qa_temp.py
@@ -4,7 +4,7 @@ Durre, I., M. J. Menne, B. E. Gleason, T. G. Houston, and R. S. Vose. 2010.
 Comprehensive Automated Quality Assurance of Daily Surface Observations. 
 Journal of Applied Meteorology and Climatology 49:1615-1633.
 
-Copyright 2014, Jared Oyler.
+Copyright 2014,2015, Jared Oyler.
 
 This file is part of TopoWx.
 
@@ -21,6 +21,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with TopoWx.  If not, see <http://www.gnu.org/licenses/>.
 '''
+from twx.utils.perf_metrics import calc_ioa_d1
 
 __all__ = ['run_qa_all','run_qa_non_spatial','run_qa_spatial_only',
            'TWX_TO_GHCN_FLAGS_MAP','GHCN_TO_TWX_FLAGS_MAP']
@@ -999,14 +1000,10 @@ def _build_lin_model(x, y):
     return slope, intercept, r_value, p_value, std_err
 
 def _get_wght_mod_temp(stn_obs, ngh_obs):
-    ioa = _calc_ioa(stn_obs, ngh_obs)
+    
+    ioa = calc_ioa_d1(stn_obs, ngh_obs)
     lin_mod = _build_lin_model(ngh_obs, stn_obs)
     return ioa, lin_mod
-
-def _calc_ioa(x, y):
-    y_mean = np.mean(y)
-    ioa = 1.0 - (np.sum(np.abs(y - x)) / np.sum(np.abs(x - y_mean) + np.abs(y - y_mean)))
-    return ioa
 
 def _stns_in_radius_mask(stn, stn_da, radius=NGH_RADIUS):
     dists = grt_circle_dist(stn[LON], stn[LAT], stn_da.stns[LON], stn_da.stns[LAT])